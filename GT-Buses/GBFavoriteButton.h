39972786e9b4b5071099de07e91512e74069932f
Alex Perez <alex@iminichrispy.com>
2014-11-12 15:36:23 -0500

    added preliminary notification center setup

---
+++ GT-Buses/GBFavoriteButton.h
@@ -0,0 +1,13 @@
+//
+//  GBFavoriteButton.h
+//  GT-Buses
+//
+//  Created by Alex Perez on 11/12/14.
+//  Copyright (c) 2014 Alex Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBFavoriteButton : UIButton
+
+@end
---
+++ GT-Buses/GBFavoriteButton.m
@@ -0,0 +1,21 @@
+//
+//  GBFavoriteButton.m
+//  GT-Buses
+//
+//  Created by Alex Perez on 11/12/14.
+//  Copyright (c) 2014 Alex Perez. All rights reserved.
+//
+
+#import "GBFavoriteButton.h"
+
+@implementation GBFavoriteButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
